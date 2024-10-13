924540e2c136610f0462f43c3b7274562dc8de2c
alexander-j-vogt <alexander@vogt-space.de>
2024-10-13 19:27:29 +0200

    WP: COUNTYLEVEL is working; it follows a search for potential issues

--- project_banking_mp_transmisison/c_program/mp_transmission_outcome.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_outcome.R
@@ -18,14 +18,14 @@ gc()
 
 
 hmda_files <- list.files(paste0(TEMP, "/") , pattern = "merge")
+start_time <- Sys.time()
+list_merge <- lapply(hmda_files, COUNTYLEVEL)
+end_time <- Sys.time()
+print(end_time - start_time)
 
-lapply
-
-
-COUNTYLEVEL <- function (filesnamex , bank_code) {
-  filenamex <- hmda_files[1]
-  bank_code <- 1 
-  year <- gsub("[^0-9]", "", filenamex)
+COUNTYLEVEL <- function (filenamex) {
+  # filenamex <- hmda_files[8]
+  year <- as.integer(gsub("[^0-9]", "", filenamex))
   # load raw hmda files
   load(paste0(TEMP, "/",filenamex))
   
@@ -38,16 +38,39 @@ COUNTYLEVEL <- function (filesnamex , bank_code) {
     stop("STOP: Only one dataset at a time!")
   }
   
-  print(paste0("Current Dataset: ", year))
-  
   # Determine based on year, which bank code needs to be used
-  if (year )
-  
+  # Over the years the HMDA has changed their codes, when it comes to indicating 
+  # who the lender is. Until 2006, mortgage-subdivisions of banks are indicated 
+  # as 1 in the variable other_lender_code. From 2007 on, additionally all
+  # depository institions were included and are listed with the numeber 0.
+  # 2007 until 2017, 
+  if (year %in% c(2000:2006)) {
+    bank_code <- c(1) # All mortgage-subdivisions of banks
+  } else if (year %in% c(2007:2017)) {
+    bank_code <- c(1, 2) # All depository instiutions and their mortgage-subdivisions
+  }
   
   # Filter for only commercial banks, who distributed mortgages + for available to 
   # state_code and county_code
   main <- main[other_lender_code %in% bank_code]
   main <- main[state_code != "" & county_code != ""]
+  
+  # Check if there any missing in the columns
+  message(paste0("Current Dataset: ", year))
+  for (colname in colnames(main)) {
+    if (any(main[[colname]] == "")) {
+      message(paste("Missing values in column:", colname))
+    } else {
+      message(paste("No missing values in column:", colname))
+    }
+  }
+  
+  # Standardize state_code and county code such that state_code consist always
+  # 2 numbers (e.g., 01 and not 1) and county_code consists of 3 numbers (e.g., 
+  # 001 and not 1)
+  main <- main[, state_code := ifelse(nchar(state_code) == 1, paste0("0", state_code), state_code)]
+  main <- main[, county_code := ifelse(nchar(county_code) == 1, paste0("00", county_code), county_code)]
+  main <- main[, county_code := ifelse(nchar(county_code) == 2, paste0("0", county_code), county_code)]
   
   # Excluding: Puerto Rico (72), US Virgin Islands (78), American Samoa (60), 
   # Northern Marian Islands (69), U.S. Minor Outlying Islands (74), Guam (66)
@@ -58,30 +81,36 @@ COUNTYLEVEL <- function (filesnamex , bank_code) {
   # main <- main[, fips := as.integer(fips)]
   
   # Clean loan amount variable is available in thousands
-  main <- main[, loan_amount := as.integer(gsub("0", "", loan_amount))]
+  main <- main[, loan_amount := as.integer(gsub("^0{1,3}", "", loan_amount))]
   
   # Keep only YEAR, fips-code and the loan amount in 000s
-  main <- main[, c("activity_year", "fips", "loan_amount")]
-  main <- main[, activity_year := as.integer(activity_year)]
+  main <- main[, year := as.integer(activity_year)]
+  main <- main[, c("year", "fips", "loan_amount")]
   
-  # Exclude all values where the fips code is incomplete # only 3134 counties instead of 3142
+  # Be sure that only 5 digit log fips code are included
   main <- main[nchar(fips) == 5]
   
   # Extract the unique year
-  year <- unique(main$activity_year)
+  # year <- unique(main$activity_year)
   
   # Collapse by county and sum up all loan amounts within a county
   main <- main[, .(total_amount_loan = sum(loan_amount)), by = fips]
   
   # Add year back to data.table
-  main <- main[, activity_year := year]
+  main <- main[, year := year]
+  
+  # Order the columns
+  main <- main[, .(year, fips, total_amount_loan)]
   
   #SAVE and assign n
-  SAVE(dfx = main, namex = paste0("hdma_county_", gsub("[^0-9]", "", files_list)))
+  SAVE(dfx = main, namex = paste0("hdma_county_", year))
+  
+  # Print to console the the process has been finished for the currrent dataset
+  message(paste("Basic Data Cleaning is finished for: ", year))
   
   # Save county-level dataset in global environment
   return(main)
   
-  # Remover raw dataset from global enviroment
-  rm(list = c(files_list))
+  # Remover raw dataset from global environment
+  rm(list = c(file_list, "main"))
 }
