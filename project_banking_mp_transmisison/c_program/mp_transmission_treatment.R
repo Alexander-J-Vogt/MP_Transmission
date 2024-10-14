8828b59027fe78f5478fc6684942c38a532750dc
alexander-j-vogt <alexander@vogt-space.de>
2024-10-14 23:02:04 +0200

    WP: Setting up control variable with SOD DF

--- project_banking_mp_transmisison/c_program/mp_transmission_treatment.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_treatment.R
@@ -1,3 +1,10 @@
+# TARGET: Creating HHI and dummy variable for dividing counties into high-market concentration and low-market concentratoin 
+# INDATA: sod_banks
+# OUTDATA/ OUTPUT: 
+
+################################################################################################################+
+# INTRO	script-specific ####
+
 #clear gobal environment of all but uppercase objects (globals, myfunctions, scalars)
 CLEARCOND()
 
@@ -12,4 +19,34 @@ MAINNAME <- substr(MAINNAME,1,nchar(MAINNAME)-2) #cut off .R
 #release unused memory 
 gc()
 
-########################
+################################################################################################################+
+# MAIN PART ####
+
+# Load the Summary of Deposits for the period 1994 to 2020
+sod <- LOAD(dfinput = "sod_banks", dfextension = ".rda")
+setDT(sod)
+
+# Select the relevant variables for creating HHI by county-level
+sod <- sod[, .(year, stcntybr, depsumbr)]
+
+
+# Create HHI by county
+
+
+# Create turnover rate of branches for each year (sims_aquired_date)
+# Create dummy variable for whether county lays in a Metropolitan Statistical Area or not (msabr)
+# Create dummy whether county is has the main office or not (bkmo)
+
+
+sod <- sod[, stcntybr := ifelse(nchar(stcntybr) == 4, paste0("0", stcntybr), "stcntybr") ]
+
+# Restrict to the period 2000 to 2020
+sod <- 
+sod <- sod[year >= 2000]
+
+ 
+test <- table(nchar(sod$stcntybr))
+
+
+
+sod$stcntybr
