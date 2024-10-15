f424c15c6d48d8f9712c8614f528430ea7771840
alexander-j-vogt <alexander@vogt-space.de>
2024-10-15 17:10:49 +0200

    WP: Calculated mean of HHI per county; change code to single dataset sod instead of sod & sod_hhi

--- project_banking_mp_transmisison/c_program/mp_transmission_treatment.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_treatment.R
@@ -37,6 +37,9 @@ sod <- sod[, bank_cnty_dep := sum(depsumbr), by = .(rssdid, fips, year)]
 # Calculate the sum of deposits by year and fips-code
 sod <- sod[, cnty_tot_dep := sum(depsumbr), by = .(fips, year)]
 
+# Check identical observation
+sod <- sod[, check_identical := bank_cnty_dep == cnty_tot_dep]
+sod[check_identical == T]
 # Calculate the market share and sqaure the value of it
 sod <- sod[, bank_market_share := bank_cnty_dep / cnty_tot_dep * 100]
 sod <- sod[, bank_market_share := ifelse(is.nan(bank_market_share), 0, bank_market_share)]
@@ -49,8 +52,48 @@ sod <- unique(sod, by = c("year", "fips", "rssdid"))
 
 # Calculate the HHI
 sod <- sod[, .(hhi = sum(bank_market_share_sq)), by = .(fips, year)]
+# sod <- sod[, check := hhi == 10000]
 
+# Create dummy variable for counties with HHI of 10000. Counties with HHI 10000
+# are associated with rural areas like counties in Alaska, Nebraska, SOuth Dakota etc,
+# There are 96 counties, which have this high market concentration over the whole period.
+sod_10000 <- sod[hhi == 10000]
+sod_10000 <- unique(sod_10000, by = c("fips", "year"))
+sod_10000 <- sod_10000[, ones := 1]
+sod_matrix <- dcast(sod_10000, fips ~ year, value.var = "ones", fill = 0)
+conc_10000 <- sod_matrix[rowSums(sod_matrix[ , 2:ncol(sod_matrix), with = FALSE] > 0) == 21]
+cnty_10000 <- sod[sod$fips %in% conc_10000$fips]
+sod <- sod[, d_hhi_10000 := ifelse(sod$fips %in% cnty_10000$fips, 1, 0)]
+
+# Calculate the mean HHI for each county for the period 2000 to 2021; based
+# on this mean the counties are divided into treatment and control group
 sod_hhi <- sod[, .(mean_hhi = mean(hhi)), by = fips]
+sod_hhi <- sod[, d_hhi_10000 := ifelse(sod$fips %in% cnty_10000$fips, 1, 0)]
+
+# Create different dummy variables for treatment and control group in the main
+# dataset "sod" with the help of sod_hhi
+# all: Includes counties with hhi == 10000
+# rest: Exldues counties with hhi == 10000
+# 1. Dummy variable with median as threshold
+median_hhi_all <- median(sod_hhi$mean_hhi)
+sod <- sod[, d_median_all := ifelse(fips %in% sod_hhi[mean_hhi > median_hhi_all, fips], 1, 0)]
+
+median_hhi_rest <- median(sod_hhi[fips conc_10000$fips ]$mean_hhi)
+median_hhi_rest <- median(sod_hhi[sod_10000 == 0])
+
+cnty_median_all <- sod_hhi[, d_median_all := ifelse(mean_hhi > median(mean_hhi), 1, 0)]
+cnty_median_all <- cnty_median_all[d_median_all == 1]
+tset <- sod[, d_median_all := ifelse(fips %in% cnty_median_all$fips, 1, 0) ]
+sod <-  d
+sod <- sod[, d_median_rest := ifelse((hhi > median(hhi)) & (d_hhi_10000 == 0), 1, 0 )]
+
+# 2. Dummy variable with mean as threshold
+# Calculate the mean of hhi over time in order to identify treatment and control group
+graph_sod <- sod_hhi
+ggplot(data = graph_sod, aes(x = mean_hhi)) +
+  geom_density(binwidth = 0.05, fill = "blue", color = "black", alpha = 0.7) +
+  labs(title = "Distribution of Mean HHI", x = "Mean HHI", y = "Frequency") +
+  theme_minimal()
 
 # Follow the strateg
 
