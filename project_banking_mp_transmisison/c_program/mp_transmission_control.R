589813e9b54b44e0f9481ad161e8527d76d4fd88
alexander-j-vogt <alexander@vogt-space.de>
2024-10-18 16:53:59 +0200

    SC: COmmented code

--- project_banking_mp_transmisison/c_program/mp_transmission_control.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_control.R
@@ -1,3 +1,10 @@
+# TARGET: Creating a Main dataset for all control variables from different sources
+# INDATA: banks_sod, pop_cnty, ur_cnty, qwi_earnings, controls_sod
+# OUTDATA/ OUTPUT: MAINNAME 
+
+################################################################################################################+
+# INTRO	script-specific ####
+
 #clear gobal environment of all but uppercase objects (globals, myfunctions, scalars)
 CLEARCOND()
 
@@ -12,9 +19,11 @@ MAINNAME <- substr(MAINNAME,1,nchar(MAINNAME)-2) #cut off .R
 #release unused memory 
 gc()
 
-########################
+################################################################################################################+
+# MAIN PART ####
 
 # 1. Summary of Deposits - Control Variables ===================================
+
 # Create control variables based on variables available in the SOD
 
 # Load the Summary of Deposits for the period 1994 to 2020
@@ -37,14 +46,18 @@ sod <- sod[, .(year, fips, sims_acquired_date, msabr, bkmo)]
 # sod <-  sod[, d_turnover := ifelse(year == sims_acquired_date, 1, NA)]
 # sod <-  sod[, cnty_turnover := sum(d_turnover), by = .(fips, year)]
 
+# Number of Main offices in a county:
 # Create dummy whether county is has the main office or not (bkmo)
 sod <-  sod[, cnty_main_office := sum(bkmo), by = .(fips, year)]
 
 # Collapse by year and fips 
 sod <- unique(sod, by = c("year", "fips"))
 
-# If a county lays in a Metropoliatian Statisistical Area or not; Areas with 
-# more than 50000 inhabitants
+# Indicatir whether a county lays in a Metropolitan Statistical Area (MSA) or not:
+# MSA tells whether an areas has than 50000 inhabitants
+# Could account for the fact that a different economic dynamic is present compared
+# to non-MSA and the amount of loans handed-out is different compared to a areas
+# with less than 50000.
 sod <- sod[, d_msa := as.integer(ifelse(msabr > 0, 1, 0))]
 
 # Select relevant variables
@@ -73,13 +86,13 @@ qwi_earnings <- LOAD(dfinput = "qwi_earnings")
 # Import Controls from sod
 controls_sod <- LOAD(dfinput = "controls_sod")
 
-# Define as data.table
-datasets <- ls()[sapply(ls(), function(x) is.data.frame(get(x)) || inherits(get(x), "data.table"))]
-print(datasets)
-
-for (dataset in datasets) {
-  assign(dataset, setDT(get(dataset)))
-}
+# # Define as data.table
+# datasets <- ls()[sapply(ls(), function(x) is.data.frame(get(x)) || inherits(get(x), "data.table"))]
+# print(datasets)
+# 
+# for (dataset in datasets) {
+#   assign(dataset, setDT(get(dataset)))
+# }
 
 # Comining datsets by county and year
 merged_data <- left_join(pop_cnty, controls_sod, by = c("fips", "year"))
