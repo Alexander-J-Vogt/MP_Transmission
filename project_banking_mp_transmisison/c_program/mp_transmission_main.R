86eac1a63c964d02910e7f486cb8b80171a8e0a6
alexander-j-vogt <alexander@vogt-space.de>
2024-10-19 11:30:28 +0200

    WP: Added a new function called COMPLETEOBS; allows to subset counties that are oberserved over the whole time period

--- project_banking_mp_transmisison/c_program/mp_transmission_main.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_main.R
@@ -22,56 +22,68 @@ gc()
 ################################################################################################################+
 # MAIN PART ####
 
+# Determine the period
+start <- 2004
+end <- 2017
+diff_year <- end - start + 1
+
+
+# 1. Dataset with Mortgages of Commercial Banks ================================
 
 # Load merged data
 banks_data <- LOAD(dfinput = "merged_banks_data")
-allfin_data <- LOAD(dfinput = "merged_allfin_data")
-# 
-
-# 1. only banks
 setDT(banks_data)
-banks_data <- banks_data[inrange(year, 2004, 2017)]
 
-# Only allow for observation with no missings
-complete_banks_data <- banks_data[complete.cases(banks_data),]
+# Subset for the relevant period
+banks_data <- banks_data[between(year, start, end)]
 
-# Get counties, which are observed over the whole time period
-check_cnty <- complete_banks_data[, c("year", "fips")]
-# check_control <- check_cnty[year == 2010]
+# # Only allow for observation with no missings
+# complete_banks_data <- banks_data[complete.cases(banks_data),]
+# 
+# # Get counties, which are observed over the whole time period
+# check_cnty <- complete_banks_data[, c("year", "fips")]
+# # check_control <- check_cnty[year == 2010]
+# 
+# # filter <- complete_data[year == 2010]
+# check_cnty <- check_cnty[, ones := 1]
+# check_cnty <- dcast(check_cnty, fips ~ year, value.var = "ones", fill = 0)
+# counties_full_obs <- check_cnty[rowSums(check_cnty[ , 2:ncol(check_cnty), with = FALSE] > 0) == diff_year]
+# 
+# complete_banks_data <- complete_banks_data[fips %in% counties_full_obs$fips]
 
-# filter <- complete_data[year == 2010]
-check_cnty <- check_cnty[, ones := 1]
-check_cnty <- dcast(check_cnty, fips ~ year, value.var = "ones", fill = 0)
-counties_full_obs <- check_cnty[rowSums(check_cnty[ , 2:ncol(check_cnty), with = FALSE] > 0) == 14]
+complete_banks_data <- COMPLETEOBS(data = banks_data, rowx = "fips", colx = "year")
 
-complete_banks_data <- complete_banks_data[fips %in% counties_full_obs$fips]
+# 2. Dataset with Mortgages of all Financial Institutions ======================
 
-
-
-
-
-
+allfin_data <- LOAD(dfinput = "merged_allfin_data")
 # 2. All fin
 setDT(allfin_data)
-allfin_data <- allfin_data[inrange(year, 2004, 2017)]
 
-# Only allow for observation with no missings
-complete_allfin_data <- allfin_data[complete.cases(allfin_data),]
+# Subset for the relevant period
+allfin_data <- allfin_data[inrange(year, start, end)]
 
-# Get counties, which are observed over the whole time period
-check_cnty <- complete_allfin_data[, c("year", "fips")]
-# check_control <- check_cnty[year == 2010]
+# # Only allow for observation with no missings
+# complete_allfin_data <- allfin_data[complete.cases(allfin_data),]
+# 
+# # Get counties, which are observed over the whole time period
+# check_cnty <- complete_allfin_data[, c("year", "fips")]
+# # check_control <- check_cnty[year == 2010]
+# 
+# # filter <- complete_data[year == 2010]
+# check_cnty <- check_cnty[, ones := 1]
+# check_cnty <- dcast(check_cnty, fips ~ year, value.var = "ones", fill = 0)
+# counties_full_obs <- check_cnty[rowSums(check_cnty[ , 2:ncol(check_cnty), with = FALSE]) == diff_year]
+# 
+# complete_allfin_data <- complete_allfin_data[fips %in% counties_full_obs$fips]
 
-# filter <- complete_data[year == 2010]
-check_cnty <- check_cnty[, ones := 1]
-check_cnty <- dcast(check_cnty, fips ~ year, value.var = "ones", fill = 0)
-counties_full_obs <- check_cnty[rowSums(check_cnty[ , 2:ncol(check_cnty), with = FALSE] > 0) == 14]
+# uniqueN(complete_allfin_data$fips)
 
-complete_allfin_data <- complete_allfin_data[fips %in% counties_full_obs$fips]
+complete_allfin_data <- COMPLETEOBS(allfin_data, rowx = "fips", colx = "year")
 
-uniqueN(complete_allfin_data$fips)
 
 # 4. Save both datasets
 SAVE(dfx = complete_allfin_data, namex = "main_allfin_data")
 
 SAVE(dfx = complete_banks_data, namex = "main_banks_data")
+
+########################## ENDE ###############################################+
