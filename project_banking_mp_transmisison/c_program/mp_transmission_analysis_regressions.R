013b2134ec47f891843b50dbdadc9ff016710204
alexander-j-vogt <alexander@vogt-space.de>
2024-10-21 18:16:32 +0200

    WP: backup before changing names

--- project_banking_mp_transmisison/c_program/mp_transmission_analysis_regressions.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_analysis_regressions.R
@@ -27,21 +27,23 @@ setDT(main)
 
 
 did_base <- felm(log(total_amount_loan) ~  d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator | 0 | 0 | 0, data = main)
-did_base_fe <- felm(log(total_amount_loan) ~  1 + d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator | state | 0 | 0, data = main)
+did_base_fe <- felm(log(total_amount_loan) ~ d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator | state | 0 | 0, data = main)
 did_base_fe_ctrl1 <- felm(log(total_amount_loan) ~  d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator + cnty_pop | state | 0 | 0, data = main)
 did_base_fe_ctrl2 <- felm(log(total_amount_loan) ~  d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator + cnty_pop + mean_earning | state | 0 | 0, data = main)
 did_base_fe_ctrl3 <- felm(log(total_amount_loan) ~  d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator + cnty_pop + mean_earning + ur| state | 0 | 0, data = main)
 did_base_fe_ctrl4 <- felm(log(total_amount_loan) ~  d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator + cnty_pop + mean_earning + ur + mean_emp | state | 0 | 0, data = main)
 did_base_fe_ctrl5 <- felm(log(total_amount_loan) ~  d_median_all + d_ffr_indicator + d_median_all * d_ffr_indicator + cnty_pop + mean_earning + ur + mean_emp + d_msa| state | 0 | 0, data = main)
 
-stargazer(did_base, did_base_fe, did_base_fe_ctrl1, did_base_fe_ctrl2, did_base_fe_ctrl3, type = "text")
+stargazer(did_base, did_base_fe, did_base_fe_ctrl1, did_base_fe_ctrl2, type = "text")
+stargazer(did_base_fe_ctrl3,did_base_fe_ctrl4, did_base_fe_ctrl5, type = "text"
+          )
 
-stargazer(did_base, did_base_fe, did_base_fe_ctrl1, did_base_fe_ctrl2, did_base_fe_ctrl3,
-          did_base_fe_ctrl4, did_base_fe_ctrl5,
+stargazer(did_base, did_base_fe, did_base_fe_ctrl1, did_base_fe_ctrl2, did_base_fe_ctrl4, did_base_fe_ctrl5,
           type = "text",
           title = "Regression Results",
-          column.labels = c("Base", "Base + FE", "Base Cntrl 1",  "Base Cntrl 2",  "Base Cntrl 3", "Base Cntrl 4", "Base Cntrl 5"),
-          dep.var.labels = "log loan amount",
+          column.labels = c("Base", "Base + FE", "Cntrl 1",  "Cntrl 2",  "Cntrl 3", "Cntrl 4", "Cntrl 5"),
+          covariate.labels = c("Dummy: HHI MC", "Dummy: Before GR", "County Pop", "Earnings", "UR", "Employment", "Dummy: MSA", "DiD Estimator")
+          # dep.var.labels = "log loan amount"
           # out = "regression_results.txt",
           # no.space = TRUE,  # Removes extra spaces for better formatting
           # digits = 2       # Rounds coefficients to 2 decimal places
