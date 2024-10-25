ec99432075ea5ac548ea6e6503c447d3daf0dc1d
alexander-j-vogt <alexander@vogt-space.de>
2024-10-25 21:38:58 +0200

    Adding share_Emp in section 7

--- project_banking_mp_transmisison/c_program/mp_transmission_databasics.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_databasics.R
@@ -508,7 +508,9 @@ SAVE(dfx = unemp_data, namex = "ur_cnty")
 
 # Import data on average earnings per county from the Quarterly Workforce Indicator
 earnings_data <- read_csv(paste0(A, "g_qwi/", "qwi_e14db0de913c427aa12de971a73eb389.csv"), col_types = cols(.default = "c"))
+pop_cnty_data <- LOAD(dfinput = "pop_cnty")
 setDT(earnings_data)
+setDT(pop_cnty_data)
 
 # Select relevant variables
 earnings_data <- earnings_data[, c("geography", "year", "quarter", "EarnBeg", "Emp")]
@@ -530,13 +532,13 @@ earnings_data <- earnings_data[!(state %in% c("72", "66", "60", "69", "74", "78"
 
 # Calculate the mean of the avergae monthly earning of a quarter in order to 
 # an annual measure for the average monthly earnings within a county and a year
-earnings_data <-  earnings_data[, avg_monthly_earn := as.integer(avg_monthly_earn)]
-earnings_data <-  earnings_data[, mean_earning := base::mean(avg_monthly_earn, na.rm = TRUE), by = .(fips, year)]
+earnings_data <- earnings_data[, avg_monthly_earn := as.integer(avg_monthly_earn)]
+earnings_data <- earnings_data[, mean_earning := base::mean(avg_monthly_earn, na.rm = TRUE), by = .(fips, year)]
 
 # Calculate the mean of the total employment of a quarter in order to 
 # an annual measure for the total employment  within a county and a year
-earnings_data <-  earnings_data[, tot_emp := as.integer(tot_emp)]
-earnings_data <-  earnings_data[, mean_emp := base::mean(tot_emp, na.rm = TRUE), by = .(fips, year)]
+earnings_data <- earnings_data[, tot_emp := as.integer(tot_emp)]
+earnings_data <- earnings_data[, mean_emp := base::mean(tot_emp, na.rm = TRUE), by = .(fips, year)]
 # test <- earnings_data[, mean_emp := zoo::na.approx(mean_emp, x = year, na.rm = TRUE), by = fips]
 
 # Collapse the data to county and year level
@@ -545,6 +547,13 @@ earnings_data <- unique(earnings_data, by = c("fips", "year"))
 # Select relevant variables
 earnings_data <- earnings_data[, c("fips", "year", "mean_earning", "mean_emp")]
 earnings_data <- earnings_data[, year := as.integer(year)]
+
+# Merge Population Data to earnings_data
+earnings_data <- merge(earnings_data, pop_cnty_data, by = c("fips", "year"))
+
+# Calculate the share of emplyoment over total county population
+earnings_data <- earnings_data[, share_emp := mean_emp / cnty_pop]
+earnings_data <- earnings_data[, c("state", "cnty_pop") := NULL]
 
 # SAVE
 SAVE(dfx = earnings_data, namex = "qwi_earnings")
@@ -563,6 +572,7 @@ gdp_data <- gdp_data[, gdp_lag := shift(gdp, type = "lag")]
 
 gdp_data <- gdp_data[, gdp_growth := (gdp - gdp_lag) / gdp_lag * 100]
 
+SAVE(dfx = gdp_data, namex = "gdp_data")
 
 
 ########################## ENDE ###############################################+
