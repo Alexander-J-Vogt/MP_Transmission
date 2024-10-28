4b6afb2ebb6805655d742bec747fb6a6948e00d3
alexander-j-vogt <alexander@vogt-space.de>
2024-10-28 14:35:28 +0100

    WIP: Adding Test for the use of clustered standard errors on the state-level

--- project_banking_mp_transmisison/c_program/mp_transmission_analysis_regressions.R
+++ project_banking_mp_transmisison/c_program/mp_transmission_analysis_regressions.R
@@ -89,6 +89,8 @@ lapply(outcome_var, function (x) {
 })
 
 
+
+
 # Dummy: Mean_pre | state FE and clustered SE ==================================
 
 lapply(outcome_var, function (x) {
@@ -118,6 +120,33 @@ lapply(outcome_var, function (x) {
   
 })
 
+# Same as with state fe model bit without clusters on the state level
+lapply(outcome_var, function (x) {
+  # x <- outcome_var[1]
+  did1 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator | 0 | 0 | 0")), data = main)
+  did2 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator | state | 0 | 0")), data = main)
+  did3 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + cnty_pop | state | 0 | 0")), data = main)
+  did4 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + mean_earning | state | 0 | 0")), data = main)
+  did5 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + ur | state | 0 | 0")), data = main)
+  did6 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + mean_emp | state | 0 | 0")), data = main)
+  did7 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + d_msa | state | 0 | 0")), data = main)
+  did8 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + cnty_pop + mean_earning + ur + mean_emp + d_msa | state | 0 | 0")), data = main)
+  did9 <- felm(as.formula(paste0(x,  "~" , "d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator +  mean_earning + ur + d_msa | state | 0 | 0")), data = main)
+  
+  stargazer(did1, did2, did3, did4, did5, did6, did7, did8, did9,
+            type = "text",
+            title = paste0("Results: ", gsub("_", " ", x)),
+            column.labels = c("Base", "FE" , "Cntrl 1",  "Cntrl 2",  "Cntrl 3", "Cntrl 4", "Cntrl 5", "Cntrl 6", "Pref."),
+            covariate.labels = c("Dummy: HHI MC", "Dummy: Before GR", "County Pop", "Earnings", "UR", "Employment", "Dummy: MSA", "DiD Estimator"),
+            dep.var.labels = c(rep(gsub("_", " ", x),9)),
+            out = paste0(LATEX, x, "_median_no_cluster.html")
+            # no.space = TRUE,  # Removes extra spaces for better formatting
+            # digits = 2       # Rounds coefficients to 2 decimal places
+  )
+  
+  # tinytex::latexmk(paste0(LATEX, x, ".tex"))
+  
+})
 
 # Dummy: Market_def | state FE and clustered SE ================================
 
@@ -324,3 +353,38 @@ lapply(outcome_var, function (x) {
   
 })
 
+# Testing the need for clustered-se ============================================
+library(sandwich)
+library(lmtest)
+
+formula1 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator)
+formula2 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + cnty_pop)
+formula3 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + lag_ur)
+formula4 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + log_earnings)
+formula5 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + d_msa)
+formula6 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + mean_emp)
+formula7 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + ur)
+formula8 <- as.formula(lead_ln_loan_amount ~ d_median_all_pre + d_ffr_indicator + d_median_all_pre:d_ffr_indicator + cnty_pop + lag_ur + ur + log_earnings + d_msa + mean_emp)
+
+formula <- c("formula1", "formula2", "formula3", "formula4", "formula5", "formula6", "formula7", "formula8" )
+
+# Hausman Test for Fixed vs Random Fixed Effects 
+pmain <- pdata.frame(main, index = c("fips", "year"))
+
+for (i in formula) {
+  fixed_model <- plm(get(i), data = pmain, model = "within")
+  random_model <- plm(get(i), data = pmain, model = "random")
+  test <- phtest(fixed_model, random_model) 
+  print(paste0("Test Results: ", i))
+  print(test)
+  # Standard errors without clustering
+  # non_clustered_se <- coeftest(fixed_model, vcov = vcovHC(fixed_model, type = "HC1"))
+  # 
+  # # Clustered standard errors at the state level
+  # clustered_se <- coeftest(fixed_model, vcov = vcovHC(fixed_model, cluster = "group"))
+  # 
+  # print(paste0("Analysis of ", i))
+  # print(non_clustered_se)
+  # print(clustered_se)
+
+}
